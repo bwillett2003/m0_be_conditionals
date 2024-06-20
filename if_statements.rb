3abcff8cdb74e6644ab829f68c9f108c3c59508b
Bryan Willett <btwillett2003@gmail.com>
2024-06-19 22:38:50 -0500

    Updates if_statements.rb to correct conditional for flour and sauce

--- if_statements.rb
+++ if_statements.rb
@@ -70,12 +70,10 @@ end
 cups_of_flour = 1
 has_sauce = true
 
-if cups_of_flour = 1 && has_sauce
-  puts "I cannot make pizza"
-elsif cups_of_flour = 5 && has_sauce = false
-  puts "I cannot make pizza"
-elsif cups_of_flour = 2 && has_sauce = true
+if cups_of_flour >= 2 && has_sauce == true
   puts "I can make pizza"
-else cups_of_flour = 3 && has_sauce = true
-  puts "I can make pizza"
+else
+  puts "I cannot make pizza"
 end
+
+
